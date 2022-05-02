3d5860d0dda7cb500c86fa006264786b50be40e7
Celian RAIMBAULT <celian.raimbault@epita.fr>
2022-05-02 13:30:25 +0200

    1: Added sqrt exercise

---
+++ exercises/1_sqrt.md
@@ -0,0 +1,13 @@
+# Sqrt
+## Statement
+Given an integer N, find its integer square root.
+
+- 0 <= N <= 10^5
+
+## Complexity
+- Time : O(log N)
+
+## Follow-up
+Given a real number X, find its square root with a precision of 10^-3.
+
+- 0 <= X <= 10^5
---
+++ solutions/1_sqrt.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int int_sqrt(int n) {
+    int l = 0;
+    int r = n;
+    while (l < r) {
+        int x = l + (r - l) / 2;
+
+        // Query whether the sqrt is lower or higher
+        int y = x * x;
+        if (y < n) {
+            l = x + 1;
+        } else {
+            r = x;
+        }
+    }
+
+    return l;
+}
+
+#include <gtest/gtest.h>
+
+TEST(Sqrt, Zero) { ASSERT_EQ(int_sqrt(0), 0); }
+
+TEST(Sqrt, One) { ASSERT_EQ(int_sqrt(1), 1); }
+
+TEST(Sqrt, Four) { ASSERT_EQ(int_sqrt(4), 2); }
+
+TEST(Sqrt, Sixteen) { ASSERT_EQ(int_sqrt(16), 4); }
+
+TEST(Sqrt, TwentyFive) { ASSERT_EQ(int_sqrt(25), 5); }
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    int ret{RUN_ALL_TESTS()};
+    return ret;
+}
